0206634b1cf71345ecfbd0738893fd1862cef124
Rômulo <romulo.gamer.br@gmail.com>
2024-05-18 22:31:26 -0300

    Parallax

--- Front-End/Projetos/WebSite/js/scripts.js
+++ Front-End/Projetos/WebSite/js/scripts.js
@@ -100,3 +100,12 @@ $( document ).ready(function() {
     }
 
   });
+
+  // Parallax
+
+  // setTimeout serve para carregar primeiro as imagens
+  setTimeout(function() {
+    $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+  }, 200);
+});
