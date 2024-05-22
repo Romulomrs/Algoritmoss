573169d57b9d5aa52c5daf4e1df45a708d1421a3
Rômulo dos Santos Mendes - 822161394 <romulo.gamer.br@gmail.com>
2024-05-22 20:16:56 -0300

    Teste

--- Front-End/Projetos/react project/src/App.js
+++ Front-End/Projetos/react project/src/App.js
@@ -9,12 +9,15 @@ function App() {
   function sum (a, b){
   return a + b
   }
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className='App'>
       <h1> Olá react</h1>
       <p>Olá {newName}</p>
       <p> soma: {sum(1,2)}</p>
       <p> Meu dededeus</p>
+      <img src={url} alt=''/>
+      <HelloWorld/>
     </div>
 
   )
--- Front-End/Projetos/react project/src/components/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-function HelloWorld(){
- return(
-    <div>
-        <h1> Meu prjimeiro Componentee</h1>
-    </div>
- )
-}
-
-export default HelloWorld;
