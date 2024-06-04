8a150bd4e6870ed2882a2b7d4ea0424a3dc9eff0
Rômulo <romulo.gamer.br@gmail.com>
2024-06-03 21:45:00 -0300

    UseState Base

--- Front-End/Projetos/react project/meu-projeto/src/components/Forms.jsx
+++ Front-End/Projetos/react project/meu-projeto/src/components/Forms.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react"
 function Forms(){
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log("cadastrou usuario")
+        console.log(name)
+        console.log(`Usuário ${name} foi cadastrado com a senha ${password}`)
     }
+    const[name, setName]=useState()
+    const[password, setpassword]=useState()
     return(
 <>
 <h1>Meu cadastro</h1>
 
+
 <form onSubmit={cadastrarUsuario}>
     <div>
-        <input type="text" placeholder="Digite seu nome"/>
+        <label htmlFor="name">Nome:</label>
+        <input
+         type="text"
+         id="name" 
+         name="name" 
+         placeholder="Digite seu nome"
+         onChange={(e)  => setName(e.target.value)}/>
     </div>
     <div>
-        <input type="submit" value="Cadastrar"/>
+    <label htmlFor="password">Senha:</label>
+        <input
+         type="password"
+         id="password" 
+         name="password" 
+         placeholder="Digite a sua senha"
+         onChange={(e)  => setpassword(e.target.value)}/>
+
     </div>
 </form>
 </>
