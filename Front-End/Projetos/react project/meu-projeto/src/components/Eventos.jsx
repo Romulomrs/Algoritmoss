454cded87e704d4bd71ccc8273bcc40556a2ff9b
Rômulo <romulo.gamer.br@gmail.com>
2024-06-09 19:49:51 -0300

    Passando eventos por props

--- Front-End/Projetos/react project/meu-projeto/src/components/Eventos.jsx
+++ Front-End/Projetos/react project/meu-projeto/src/components/Eventos.jsx
@@ -1,11 +1,16 @@
-function Evento ({numero}){
+import Button from './Eventos/button'
+function Evento (){
     function meuEvento(){
         console.log(`Opa, fui ativado ${numero}`)
     }
+    function SegundoEvento(){
+        console.log('Ativando segundo evento')
+}
         return(
             <>
             <p>Clique para disparar um evento</p>
-            <button onClick={meuEvento}>Ativar</button>
+            <Button event={meuEvento}text= "primeiro evento"/>
+            <Button event={SegundoEvento}text= "primeiro evento"/>
             </>
 
     )
